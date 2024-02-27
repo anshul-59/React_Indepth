d620717e1d015c43e40f1684b65b34e262d145d6
anshul-59 <anshul.bansod5901@gmail.com>
2024-02-27 16:10:51 +0530

    Readt Deep Dive

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading    " },
+  "Hellow world from react"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
